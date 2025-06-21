d75ebce30a8a3e7de1104804e0f65cc703149ec5
Farhan <ahmad.farhan9715@gmail.com>
2025-06-21 18:50:18 +0100

    changed to task

--- dags/assets_mysql.py
+++
@@ -1,9 +0,0 @@
-# dags/assets_mysql.py
-
-from airflow.sdk import Asset
-
-# *Changed* uri to point at our local-docker connection "mysql_local"
-mysql_table_asset = Asset(
-    uri="mysql://host.docker.internal/test_db/testtable",
-    name="my_table_asset",
-)
